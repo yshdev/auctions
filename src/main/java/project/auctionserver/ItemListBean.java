e2baa622b9ae624bdf0617037e2c5d32494ecda3
AharonGross <60959336+AharonGross@users.noreply.github.com>
2020-03-22 11:27:24 +0200

    auction lists

--- src/main/java/project/auctionserver/ItemListBean.java
+++ src/main/java/project/auctionserver/ItemListBean.java
@@ -1,13 +1,13 @@
 package project.auctionserver;
 
 import project.service.CategoryDto;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import project.dal.UnitOfWork;
-import project.domain.Category;
-import project.domain.Auction;
+import project.domain.*;
 
 @Named(value = "itemListBean")
 @SessionScoped
@@ -20,6 +20,11 @@ public class ItemListBean implements Serializable {
     private Category category;
     private String sortOption;
     private List<Auction> chosenList;
+    private List<Auction> itemsActiveList = new ArrayList<>();
+    private List<Auction> bidsActiveList = new ArrayList<>();
+    private List<Auction> itemsClosedList = new ArrayList<>();
+    private List<Auction> bidsClosedList = new ArrayList<>();
+    private Auction chosenAuction;
 
     public ItemListBean() {
     }
@@ -32,7 +37,11 @@ public class ItemListBean implements Serializable {
         this.sortOption = sortOption;
     }
     
-    public Category getCategory() {
+    public void setChosenAuction(Auction chosenAuction) {
+        this.chosenAuction = chosenAuction;
+    }
+    
+   public Category getCategory() {
         return category;
     }
     
@@ -48,6 +57,26 @@ public class ItemListBean implements Serializable {
         return chosenList;
     }
     
+    public List<Auction> getItemsActiveList() {
+        return itemsActiveList;
+    }
+    
+    public List<Auction> getBidsActiveList() {
+        return bidsActiveList;
+    }
+    
+    public List<Auction> getItemsClosedList() {
+        return itemsClosedList;
+    }
+    
+    public List<Auction> getBidsClosedList() {
+        return bidsClosedList;
+    }
+    
+    public Auction getChosenAuction() {
+        return chosenAuction;
+    }
+    
     public CategoryDto[] getCategories() {
         try (UnitOfWork unitOfWork = UnitOfWork.create()) {
            CategoryDto[] categories = unitOfWork.getAllCategories();
@@ -55,9 +84,31 @@ public class ItemListBean implements Serializable {
         }
     }
     
-    public void createList() {
+    public void createLists(UserProfile connectedUser) {
         UnitOfWork unitOfWork = UnitOfWork.create();
         chosenList = unitOfWork.getSortedList(category, sortOption);
         unitOfWork.close();
+        
+        for (Auction auction : chosenList) {
+            if (auction.getOwner() == connectedUser) {
+                if (auction.isIsClosed())
+                    itemsClosedList.add(auction);
+                else
+                    itemsActiveList.add(auction);
+            }
+            for (Bid bid : auction.getBids()) {
+                if (bid.getBidder() == connectedUser) {
+                    if (auction.isIsClosed())
+                        itemsClosedList.add(auction);
+                    else
+                        itemsActiveList.add(auction);
+                }
+            }
+        }
+    }
+    
+    public String displayItem(Auction auction) {
+        this.chosenAuction = auction;
+        return "itemDisplay.xhtml";
     }
 }
